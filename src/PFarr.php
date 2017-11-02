a719637d1c53ec5955951b126b867c31117f6af8
LXZ <Lampxiezi@163.com>
2017-11-02 14:50:26 +0800

    update

--- src/PFarr.php
+++ src/PFarr.php
@@ -7,27 +7,27 @@ class PFarr {
     protected $pf_array_link;
     protected function driver()
     {
-        $this->link = new Base();
+        $this->pf_array_link = new Base();
         return $this;
     }
 
     public function __call($method, $params)
     {
-        if (is_null($this->link)) {
+        if (is_null($this->pf_array_link)) {
             $this->driver();
         }
-        if (method_exists($this->link, $method)) {
-            return call_user_func_array([$this->link, $method], $params);
+        if (method_exists($this->pf_array_link, $method)) {
+            return call_user_func_array([$this->pf_array_link, $method], $params);
         }
     }
 
     public static function single()
     {
-        static $link;
-        if (is_null($link)) {
-            $link = new static();
+        static $pf_array_link;
+        if (is_null($pf_array_link)) {
+            $pf_array_link = new static();
         }
-        return $link;
+        return $pf_array_link;
     }
 
     public static function __callStatic($name, $arguments)
